e66ded01956d34708cb9e9c33b6f0d483fc0bcc3
Jefaxe <73656286+Jefaxe@users.noreply.github.com>
2021-03-13 09:08:06 +0000

    Update downjef.py

--- downjef.py
+++ downjef.py
@@ -12,7 +12,7 @@ def run(forceUpdate=False):
         # Download the file from `url` and save it locally under `file_name`:
         with urllib.request.urlopen("https://raw.githubusercontent.com/Jefaxe/jeflib/main/jeflib.py") as response, open("libs/jeflib.py", 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
-     else:
+    else:
         if not os.path.exists("libs"):
             os.mkdir("libs")
         # Download the file from `url` and save it locally under `file_name`:
