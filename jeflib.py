21ce95b7af6fba465574e44f85ab3d18c608fe64
Jefaxe <73656286+Jefaxe@users.noreply.github.com>
2021-03-13 08:55:45 +0000

    Updated jeflib.py
    removed `requests` package and switched to urllib.request

--- jeflib.py
+++ jeflib.py
@@ -1,12 +1,12 @@
 #this libary was created by Jefaxe
-#it includes some handy function, to use thislibary simply download it, put it into the folder your running your script from
+#it includes some handy function, to use this libary simply download it, put it into the folder your running your script from
 #write "import jeflib" then use jeflib.<function> to call functions.
+#or use downjef at the github page to automate the download, and use "import libs.jeflib as jb", then use "jb.<function>"
 
 import os
-import requests
-import urllib
+import urllib.request #why do both need to be imported, isn't urllib.requst part of urllib?
 import logging
-
+import shutil
 
 #exceptions
 class WebError(Exception):
@@ -30,67 +30,69 @@ def clearFile(name):
    else:
        with open(name,"w") as recreate:
            pass
-def connection():
+def connected():
     try:
         urllib.request.urlopen("https://google.com") #Python 3.x
         return True
     except Exception as e:
-        logging.error("No internet")
         return False
 
-def downloadFile(url,name="|",overwrite=False,crash_at_404=True,crash_at_no_internet=False,crash_at_permissionError=False): #makes it easier to download files, only downloads them if they are not already downloaded (unless overwite=True is set)
+def downloadFile(url,name="|",overwrite=False,NotFoundCrash=True,LostConnectionCrash=True): #makes it easier to download files, only downloads them if they are not already downloaded (unless overwite=True is set)
     if name=="|":
         from urllib.parse import urlparse
         p = urlparse(url)
         name=p.path.rsplit("/", 1)[-1]
-    if not connection():
-        if crash_at_no_internet:
+    if not connected():
+        if LostConnectionCrash:
             raise WebError("No internet")
         else:
-            logging.info("no internt, not downloading file (this can be disabled with 'crash_at_no_internet=True'")
+            logging.error("No internt")
+            return False
     if not overwrite:
         if not os.path.exists(name):
             try:
-                r = requests.get(url)
-            except requests.exceptions.MissingSchema:
-                r = requests.get("http://"+url)
-            try:
-                if not r.content.decode("utf-8")=="404: Not Found":
-                    try:
-                        with open(name,'wb') as output_file:
-                            output_file.write(r.content)
-                    except FileNotFoundError: #as in directory not found
-                        os.makedirs(os.path.split(name)[0])
-                        with open(filepath,'wb') as output_file:
-                            output_file.write(r.content)
-                    except PermissionError: #most likely trying to read a directory
-                        if crash_at_permissionError:
-                            raise WebError("You tried to read the directory "+name+" or you don't have write-permission here")
-                        else:
-                            logging.warn("You tried to read the directory "+name+" or you don't have write-permission here")
-                            return False
-                    return True
-                elif r.content.decode("utf-8")=="404: Not Found" and crash_at_404:
-                    raise WebError("The file said to be at "+url+" returned 404")
-                    return False
-                elif r.content.decode("utf-8")=="404: Not Found" and not crash_at_404:
-                    logging.error(name+" could not be found at "+url)
-                    return False
-            except UnicodeError:
                 try:
-                    with open(name,'wb') as output_file:
-                        output_file.write(r.content)
-                except FileNotFoundError: #as in directory not found
-                    #logging.debug(os.path.split(name))
-                    os.makedirs(os.path.split(name)[0])
-                    with open(name,'wb') as output_file:
-                        output_file.write(r.content)
-                return True
+                    # Download the file from `url` and save it locally under `name`:
+                    with urllib.request.urlopen(url) as response, open(name, 'wb') as out_file:
+                        shutil.copyfileobj(response, out_file)
+                except ValueError: #if http / https is not supplied, give http (will redirect to https if availiable)
+                    with urllib.request.urlopen("http://"+url) as response, open(name, 'wb') as out_file:
+                        shutil.copyfileobj(response, out_file)
+                with open(name,"rb") as contents:
+                    contents=contents.read()
+            except urllib.error.HTTPError as e:
+                if not NotFoundCrash:
+                    logging.error(url+" is not a file")
+                else:
+                    raise WebError(url+" is not a file")
+            except urllib.error.URLError:
+                if not NotFoundCrash:
+                    logging.error(url+"'s host was not found")
+                else:
+                    raise WebError(url+"'s host was not found")
         else:
             logging.debug("You already have, "+name+", not downloading (force download by setting overwrite=True)")
     if overwrite:
-        with open(name,'wb') as output_file:
-                        output_file.write(r.content)
+        try:
+                try:
+                    # Download the file from `url` and save it locally under `name`:
+                    with urllib.request.urlopen(url) as response, open(name, 'wb') as out_file:
+                        shutil.copyfileobj(response, out_file)
+                except ValueError: #if http / https is not supplied, give http (will redirect to https if availiable)
+                    with urllib.request.urlopen("http://"+url) as response, open(name, 'wb') as out_file:
+                        shutil.copyfileobj(response, out_file)
+                with open(name,"rb") as contents:
+                    contents=contents.read()
+            except urllib.error.HTTPError as e:
+                if not NotFoundCrash:
+                    logging.error(url+" is not a file")
+                else:
+                    raise WebError(url+" is not a file")
+            except urllib.error.URLError:
+                if not NotFoundCrash:
+                    logging.error(url+"'s host was not found")
+                else:
+                    raise WebError(url+"'s host was not found")
 
 def createFile(filename,contents,overwite=False): #creates a file if it does not exits, otherwise does nothing
     if not os.path.exists(filename) or overwite==True:
